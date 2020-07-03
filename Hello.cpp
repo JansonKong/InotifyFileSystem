aaf000ae48f4f074aeaa821023b5b08964575f0a
JansonKong <995710545@qq.com>
2020-07-03 13:48:20 +0800

    init project

---
+++ Hello.cpp
@@ -0,0 +1,19 @@
+/**
+ *Copyright:SCUT
+ *Author:kjs
+ *Date:2020/5/22
+ *Description: excel file
+ *File: Hello.cpp
+*/
+
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout<< argv[1] << " synchronous file ...."  << endl;
+    sleep(5);
+    return 0;
+}
+
